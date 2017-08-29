6d95673e63ab911b7a0bcabcdaa27b3182fc5c93
Emmanuel Lehmann <emmanuel.lehmann@axa.fr>
2017-08-29 11:26:42 +0200

    feat(java) : ajout template java

---
+++ Java/src/test/java/gherkin/steps/GestionDesVirementsThenSteps.java
@@ -0,0 +1,17 @@
+package gherkin.steps;
+
+import cucumber.api.java.en.Then;
+import org.junit.Assert;
+import presz.bddtdd.tests.java.model.Context;
+//import presz.bddtdd.tests.java.process.RetourVirement;
+
+public class GestionDesVirementsThenSteps {
+
+    private Context context;
+
+    public GestionDesVirementsThenSteps(Context ctx) {
+        this.context = ctx;
+    }
+
+   
+}
---
+++ Java/src/test/java/presz/bddtdd/tests/java/process/ServiceVirementDevraitTest.java
@@ -0,0 +1,17 @@
+package presz.bddtdd.tests.java.process;
+
+import org.junit.Test;
+import org.junit.Assert;
+//import presz.bddtdd.tests.java.model.Compte;
+
+public class ServiceVirementDevraitTest {
+
+    @Test
+    public void NeRienFaire() throws Exception {
+        //Arrange
+
+        //Act
+       
+        //Assert
+    }   
+}
